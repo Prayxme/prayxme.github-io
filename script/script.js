9e73876c5c961f061c6b0f6fa3b84b8256dc62bc
Julian Suarez <julianjose2511@gmail.com>
2023-07-02 14:52:02 -0400

    nueva confirmacion

--- script/script.js
+++ script/script.js
@@ -1,6 +1,37 @@
-/*======== sticky navbar ========*/
+/*======== scroll sections active link ========*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
 window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+
+
+/*======== swiper ========*/    
 let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
 };
+
+
+/*======== swiper ========*/
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 50,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+  });
